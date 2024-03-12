dd84b44cababf21da923f10d8d71235f7e4a7269
Ruveyda Hilal  Akçınar <rhakcinar@gmail.com>
2024-03-12 12:26:25 +0300

    Created TestNG projects

--- SeleniumFirstProject/src/test/java/TestNG_Groups.java
+++ SeleniumFirstProject/src/test/java/TestNG_Groups.java
@@ -1,3 +1,20 @@
-//public class TestNG_Groups {
-//    @Test
-//}
+import org.testng.annotations.Test;
+
+public class TestNG_Groups {
+    @Test(groups = {"Group A"})
+    public void test1(){
+        System.out.println("Test1");
+    }
+    @Test(groups = {"Group B"})
+    public void test2(){
+        System.out.println("Test2");
+    }
+    @Test(groups = {"Group A","Group B"})
+    public void test3(){
+        System.out.println("Test3");
+    }
+    @Test(groups = {"Group C"})
+    public void test4(){
+        System.out.println("Test4");
+    }
+}
